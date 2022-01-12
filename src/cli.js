12621b183ff7bb6ece7df425ae7cf9f32be73855
Ion Mocan <ionatanmocan@gmail.com>
2022-01-12 02:03:43 +0200

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 // Wait for user's response.
 
-export default function () {
-  var userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + userName + '!');
-}
+const greet = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
+export default greet;
