e05e79c925d5f6ab3cfd188740b43c54e51efd48
Ion Mocan <ionatanmocan@gmail.com>
2022-01-14 14:24:12 +0200

    fix bug #3: fixed magic numbers in brain-primes.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,25 +1,25 @@
 import startGame from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
+function isPrime(n) {
+  if (Number.isNaN(n) || !Number.isFinite(n) || n % 1 || n < 2) return 'no';
+  if (n % 2 === 0) return 'no';
+  if (n % 3 === 0) return 'no';
+  const squareRootOfN = Math.sqrt(n);
+  for (let i = 5; i <= squareRootOfN; i += 6) {
+    if (n % i === 0) return 'no';
+    if (n % (i + 2) === 0) return 'no';
+  }
+  return 'yes';
+}
+
 const rules = {
   gameRule: 'Answer "yes" if given number is prime. Otherwise answer "no".',
   generatedQuestion() {
     return getRandomNumber(1000);
   },
   getRightAnswer(question) {
-    if (question <= 3) return question > 1;
-
-    if (question % 2 === 0 || question % 3 === 0) return 'no';
-
-    let count = 5;
-
-    while (count ** 2 <= question) {
-      if (question % count === 0 || question % (count + 2) === 0) return 'no';
-
-      count += 6;
-    }
-
-    return 'yes';
+    return isPrime(question);
   },
 };
 
