84b7240abb8bf4f11cdf101888d9de056ff5f121
Ion Mocan <ionatanmocan@gmail.com>
2022-01-19 02:46:33 +0200

    refactored function by splitting in two functions

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,26 +3,43 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const rule = 'What number is missing in the progression?';
 
-const getGameQuestionAndAnswer = () => {
-  const progressionArrayLength = getRandomNumber(5, 15);
-  let initialNumber = getRandomNumber(0, 10);
-  const step = getRandomNumber(1, 10);
-  const randomIndexInArray = getRandomNumber(0, progressionArrayLength - 1);
+const getProgressionAndSecretValue = ({
+  initialNumber,
+  step,
+  length,
+  randomIndex,
+}) => {
+  let firstNumber = initialNumber;
   const progressionArray = [];
   let gameAnswer;
-  for (let i = 0; i < progressionArrayLength; i += 1) {
-    if (i === randomIndexInArray) {
-      progressionArray.push('..');
-      gameAnswer = initialNumber.toString();
-      initialNumber += step;
+  for (let i = 0; i < length; i += 1) {
+    let itemToBeAdded;
+    if (i === randomIndex) {
+      itemToBeAdded = '..';
+      gameAnswer = firstNumber.toString();
     } else {
-      progressionArray.push(initialNumber);
-      initialNumber += step;
+      itemToBeAdded = firstNumber;
     }
+    progressionArray.push(itemToBeAdded);
+    firstNumber += step;
   }
-
   const gameQuestion = progressionArray.join(' ');
+  return [gameQuestion, gameAnswer];
+};
 
+const getGameQuestionAndAnswer = () => {
+  const progressionArrayLength = getRandomNumber(5, 15);
+  const initialNumber = getRandomNumber(0, 10);
+  const step = getRandomNumber(1, 10);
+  const randomIndex = getRandomNumber(0, progressionArrayLength - 1);
+  const progressionArray = getProgressionAndSecretValue({
+    initialNumber,
+    step,
+    length: progressionArrayLength,
+    randomIndex,
+  });
+
+  const [gameQuestion, gameAnswer] = progressionArray;
   return [gameQuestion, gameAnswer];
 };
 
