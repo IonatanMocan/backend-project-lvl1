1fe445aa19f96ef4feb4822af1c0ef43ce0a3855
Ion Mocan <ionatanmocan@gmail.com>
2022-01-19 02:46:11 +0200

    refactored function with switch statement

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -7,15 +7,24 @@ const getGameQuestionAndAnswer = () => {
   const firstNum = getRandomNumber(0, 10);
   const secondNum = getRandomNumber(0, 10);
   const operations = ['+', '-', '*'];
-  const sign = operations[getRandomNumber(0, 2)];
+  const sign = operations[getRandomNumber(0, operations.length - 1)];
 
   let result;
-  if (sign === '+') {
-    result = firstNum + secondNum;
-  } else if (sign === '-') {
-    result = firstNum - secondNum;
-  } else {
-    result = firstNum * secondNum;
+  switch (sign) {
+    case '+':
+      result = firstNum + secondNum;
+      break;
+
+    case '-':
+      result = firstNum - secondNum;
+      break;
+
+    case '*':
+      result = firstNum * secondNum;
+      break;
+
+    default:
+      throw new Error('unhandled case for an arithmetic operator');
   }
   const gameQuestion = `${firstNum} ${sign} ${secondNum}`;
   const gameAnswer = result.toString();
